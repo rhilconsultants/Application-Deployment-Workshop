83b0f9cf34b112eb7756d1a4714ff0d444bf6d83
Tekton Pipeline <tekton@pipeline.com>
2022-11-30 16:12:13 +0200

    added dockerfile

--- src/app.js
+++ src/app.js
@@ -2,22 +2,27 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+var port = process.env.PORT
 
+
+// Route application to index.html file.
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/html/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
+// Health Probe - Application Liveliness
 router.get('/health/liveliness',function(req,res){
     res.status(200).send('Healty')
   });
 
+// Health Probe - Application Readiness
 router.get('/health/readiness',function(req,res){
     res.status(200).send('Ready')
   });  
 
 //add the router
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(port);
 
-console.log('Running at Port 3000');
+console.log(`Running at Port ${port}`);
